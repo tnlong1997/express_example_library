30a09be2e664176e06ac5dc763b060c92089e2df
tnlong1997 <tnlong1997@gmail.com>
2018-03-19 23:23:10 -0400

    Working on genre detail page

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,4 +1,6 @@
 var Genre = require('../models/genre');
+var Book = require('../models/book');
+var async = require('async');
 
 // Display list of all Genre.
 exports.genre_list = function(req, res) {
